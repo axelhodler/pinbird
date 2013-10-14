b9026ec805f702c39a715a668d10f0af5b2694f5
xorrr <derxor@gmail.com>
2013-10-14 12:56:40 +0200

    create basic bookmark pojo

---
+++ src/main/java/earth/xor/Bookmark.java
@@ -0,0 +1,26 @@
+package earth.xor;
+
+public class Bookmark {
+
+    private String title;
+    private String url;
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getUrl() {
+        return this.url;
+    }
+
+
+
+}
