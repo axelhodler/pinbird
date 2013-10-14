05eef76a35f31f6309e7ddd5c22c12a242400ac7
xorrr <derxor@gmail.com>
2013-10-14 17:14:57 +0200

    correct tests and refactor

---
+++ src/test/java/earth/xor/DbProperties.java
@@ -0,0 +1,6 @@
+package earth.xor;
+
+public class DbProperties {
+    public static final String DB_NAME = "test";
+    public static final int EMBEDDED_PORT = 12345;
+}
--- src/test/java/earth/xor/db/TestBookmarksCollection.java
+++ src/test/java/earth/xor/db/TestBookmarksCollection.java
@@ -1,24 +1,43 @@
 package earth.xor.db;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 import java.io.IOException;
 import java.net.UnknownHostException;
 
 import org.junit.Test;
 
+import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
+
+import earth.xor.DbProperties;
 
 public class TestBookmarksCollection {
 
     @Test
     public void testAccessingTheEmbeddedMongoClient()
             throws UnknownHostException, IOException {
-        EmbeddedMongo m = new EmbeddedMongo();
-        m.launchEmbeddedMongo(12345);
+        launchEmbeddedMongo();
 
-        MongoClient c = new MongoClient("localhost", 12345);
-        assertTrue(c.getDB("test") instanceof DB);
+        DBObject dbo = createAndFindDocument();
+
+        assertEquals("bar", dbo.get("foo"));
+    }
+
+    private DBObject createAndFindDocument() throws UnknownHostException {
+        MongoClient c = new MongoClient("localhost", DbProperties.EMBEDDED_PORT);
+        DB db = c.getDB(DbProperties.DB_NAME);
+        DBCollection col = db.getCollection(DbProperties.DB_NAME);
+        col.insert(new BasicDBObject("foo", "bar"));
+        DBObject dbo = col.findOne(new BasicDBObject("foo", "bar"));
+        return dbo;
+    }
+
+    private void launchEmbeddedMongo() throws UnknownHostException, IOException {
+        EmbeddedMongo m = new EmbeddedMongo();
+        m.launchEmbeddedMongo(DbProperties.EMBEDDED_PORT);
     }
 }
