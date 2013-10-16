5c72752e09715b5c4cd9c2d4f35239c6f914edf4
xorrr <derxor@gmail.com>
2013-10-16 19:00:55 +0200

    add 3 BasicDBObjects into TestValues

--- src/test/java/earth/xor/rest/TestValues.java
+++ src/test/java/earth/xor/rest/TestValues.java
@@ -1,9 +1,25 @@
 package earth.xor.rest;
 
+import com.mongodb.BasicDBObject;
+
+import earth.xor.db.DbProperties;
+
 public class TestValues {
 
     private TestValues() {
     }
 
     public static final String POST_BOOKMARK_1 = "{\"title\":\"foo\", \"url\":\"http://www.foo.org\"}";
+
+    public static final BasicDBObject BOOKMARK_1 = new BasicDBObject(
+            DbProperties.TITLE, "foo").append(DbProperties.URL,
+            "http://www.foo.org");
+
+    public static final BasicDBObject BOOKMARK_2 = new BasicDBObject(
+            DbProperties.TITLE, "bar").append(DbProperties.URL,
+            "http://www.bar.org");
+
+    public static final BasicDBObject BOOKMARK_3 = new BasicDBObject(
+            DbProperties.TITLE, "baz").append(DbProperties.URL,
+            "http://www.baz.org");
 }
