2e572fb1c5c0b6d2596c82f7db6ad07e09a2de49
xorrr <derxor@gmail.com>
2013-10-16 19:47:11 +0200

    ConfigAccessor is now a singleton

--- src/main/java/earth/xor/db/ConfigAccessor.java
+++ src/main/java/earth/xor/db/ConfigAccessor.java
@@ -5,23 +5,30 @@ import java.io.IOException;
 import java.util.Properties;
 
 public class ConfigAccessor {
-    
-    private Properties properties;
-    
-    public ConfigAccessor() {
-        this.properties = new Properties();
-        try {
-            this.properties.load(new FileInputStream("config.properties"));
-        } catch (IOException e) {
-            e.printStackTrace();
+
+    private static Properties properties;
+    private static ConfigAccessor instance = null;
+
+    private ConfigAccessor() {}
+
+    public static ConfigAccessor getInstance() {
+        if (instance == null) {
+            instance = new ConfigAccessor();
+            properties = new Properties();
+            try {
+                properties.load(new FileInputStream("config.properties"));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
+        return instance;
     }
-    
+
     public String getMongoUri() {
-        return this.properties.getProperty("uri");
+        return properties.getProperty("uri");
     }
-    
+
     public String getDatabaseName() {
-        return this.properties.getProperty("dbname");
+        return properties.getProperty("dbname");
     }
 }
