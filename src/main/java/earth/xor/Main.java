5064fc663dc6059c21febc2b5fa2a87d33d366bc
xorrr <derxor@gmail.com>
2013-10-16 19:48:12 +0200

    add main class as the entry point of the application

---
+++ src/main/java/earth/xor/Main.java
@@ -0,0 +1,22 @@
+package earth.xor;
+
+import java.net.UnknownHostException;
+
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+
+import earth.xor.db.ConfigAccessor;
+import earth.xor.rest.RestServer;
+
+public class Main {
+
+    public static void main(String args[]) throws UnknownHostException {
+
+        MongoClientURI mongoUri = new MongoClientURI(ConfigAccessor
+                .getInstance().getMongoUri());
+        MongoClient client = new MongoClient(mongoUri);
+
+        RestServer rest = new RestServer(client);
+        rest.start();
+    }
+}
