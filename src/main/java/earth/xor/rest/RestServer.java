ff45e9b04f5848262e70796ee481f5569ce9d3be
xorrr <derxor@gmail.com>
2013-10-16 17:29:57 +0200

    add POST route bookmark to server

--- src/main/java/earth/xor/rest/RestServer.java
+++ src/main/java/earth/xor/rest/RestServer.java
@@ -1,9 +1,21 @@
 package earth.xor.rest;
 
 import static spark.Spark.get;
+import static spark.Spark.post;
+
+import java.net.UnknownHostException;
+
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
+
 import spark.Request;
 import spark.Response;
 import spark.Route;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.MongoClient;
+
+import earth.xor.DbProperties;
 
 public class RestServer {
 
@@ -14,6 +26,29 @@ public class RestServer {
             public Object handle(Request request, Response response) {
                 return "hello world";
             }
+        });
+
+        post(new Route("/bookmark") {
+
+            @Override
+            public Object handle(Request request, Response response) {
+                JSONObject obj = (JSONObject) JSONValue.parse(request.body());
+
+                MongoClient client = null;
+                try {
+                    client = new MongoClient("localhost", 12345);
+                } catch (UnknownHostException e) {
+                    e.printStackTrace();
+                }
+
+                BasicDBObject dbo = new BasicDBObject(DbProperties.TITLE,
+                        obj.get(DbProperties.TITLE)).append(DbProperties.URL,
+                        obj.get(DbProperties.URL));
+                client.getDB(DbProperties.DB_NAME)
+                        .getCollection(DbProperties.COL_NAME).insert(dbo);
+                return obj.toJSONString();
+            }
+
         });
     }
 }
--- src/test/java/earth/xor/rest/TestRestServer.java
+++ src/test/java/earth/xor/rest/TestRestServer.java
@@ -1,10 +1,21 @@
 package earth.xor.rest;
 
-import static com.jayway.restassured.RestAssured.*;
+import static com.jayway.restassured.RestAssured.expect;
+import static com.jayway.restassured.RestAssured.given;
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.net.UnknownHostException;
 
 import org.junit.Test;
 
 import com.jayway.restassured.RestAssured;
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBObject;
+import com.mongodb.MongoClient;
+
+import earth.xor.DbProperties;
+import earth.xor.db.EmbeddedMongo;
 
 public class TestRestServer {
 
@@ -15,5 +26,24 @@ public class TestRestServer {
 
         RestAssured.port = 4567;
         expect().get("/hello").body().asString().contains("hello world");
+    }
+
+    @Test
+    public void testAddingABookmark() throws UnknownHostException, IOException {
+        RestServer rs = new RestServer();
+        rs.start();
+
+        EmbeddedMongo.startEmbeddedMongo(12345);
+        RestAssured.port = 4567;
+        given().body("{\"title\":\"foo\", \"url\":\"http://www.foo.org\"}")
+                .expect().contentType("application/json").when()
+                .post("/bookmark");
+
+        MongoClient client = new MongoClient("localhost", 12345);
+        DBObject dbo = client.getDB(DbProperties.DB_NAME)
+                .getCollection(DbProperties.COL_NAME)
+                .findOne(new BasicDBObject("title", "foo"));
+
+        assertEquals("foo", dbo.get("title").toString());
     }
 }
