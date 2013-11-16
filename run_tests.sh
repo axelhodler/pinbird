29251ddca92401352bd3da0e8c4804554227ae99
xorrr <derxor@gmail.com>
2013-11-17 00:18:45 +0100

    add shell script to run tests on a port of the users choice

---
+++ run_tests.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Call this script with one parameter for the PORT variable, for example:
+# ./run_tests.sh 5000
+
+if [ -z "$1"]
+then
+    echo -e "please provide the port as a parameter.\nExample usage:\n./run_tests.sh 1337"
+    exit 0
+fi
+
+echo "run tests with port $1"
+
+export PORT=$1
+
+mvn test
