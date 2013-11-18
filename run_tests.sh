976cb63407975f68a0d6d7cf18945055348cc418
xorrr <derxor@gmail.com>
2013-11-18 11:30:11 +0100

    fix run_tests.sh script syntax

--- run_tests.sh
+++ run_tests.sh
@@ -2,8 +2,7 @@
 # Call this script with one parameter for the PORT variable, for example:
 # ./run_tests.sh 5000
 
-if [ -z "$1"]
-then
+if [ -z "$1" ]; then
     echo -e "please provide the port as a parameter.\nExample usage:\n./run_tests.sh 1337"
     exit 0
 fi
