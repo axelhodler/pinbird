e48ed3bb2667d2fc2a7a49ec25ef36c85fa7cc68
xorrr <derxor@gmail.com>
2013-11-20 00:24:23 +0100

    add MONGO_URI to env variables and change run script accordingly

--- run.sh
+++ run.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
+echo -e "run server on PORT: $1 and\nMongoClientURI: $2"
+
+export PORT=$1
+export MONGO_URI=$2
+
 mvn exec:java -Dexec.mainClass="earth.xor.Main"
--- src/main/java/earth/xor/Main.java
+++ src/main/java/earth/xor/Main.java
@@ -5,15 +5,13 @@ import java.net.UnknownHostException;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
 
-import earth.xor.db.ConfigAccessor;
 import earth.xor.rest.RestServer;
 
 public class Main {
 
     public static void main(String args[]) throws UnknownHostException {
 
-        MongoClientURI mongoUri = new MongoClientURI(ConfigAccessor
-                .getInstance().getMongoUri());
+        MongoClientURI mongoUri = new MongoClientURI(System.getenv("MONGO_URI"));
         MongoClient client = new MongoClient(mongoUri);
 
         RestServer rest = new RestServer(client);
