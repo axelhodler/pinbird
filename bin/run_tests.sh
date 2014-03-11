6c55d1f5ba5d767ffb0d3acea3eee77dc45d5494
xorrr <derxor@gmail.com>
2014-03-11 01:15:51 +0100

    remove complexity from run_tests script

--- bin/run_tests.sh
+++ bin/run_tests.sh
@@ -1,21 +1,8 @@
-#!/usr/bin/zsh
-# Call this script with one parameter for the PORT variable, for example:
-# ./run_tests.sh 5000
+#!/bin/sh
 
-source ~/.zshrc
+export PORT=5000
+export MONGO_PORT=12345
+export MONGO_URI=mongodb://localhost:12345
+export DB_NAME=test
 
-if [ -z "$1" ]; then
-    echo -e "please provide the port as a parameter.\nExample usage:\n./run_tests.sh 1337"
-    exit 0
-fi
-
-echo -e "run tests with serverport: $1\nEmbedded Mongo Port: $2\nUri: $3\nDatabase Name: $4\n"
-
-export PORT=$1
-# The port used for the EmbeddedMongo instance eg: "12345"
-export MONGO_PORT=$2
-# The URI for the MongoClient eg: "mongodb://localhost:12345"
-export MONGO_URI=$3
-
-export DB_NAME=$4
-eval "mvn test"
+mvn test
